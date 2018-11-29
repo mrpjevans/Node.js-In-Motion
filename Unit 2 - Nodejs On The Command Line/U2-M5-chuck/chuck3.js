5ce56a27af0478e62f8c833d824cc30c2cb58ff9
PJ Evans <pj@mrpjevans.com>
2018-11-29 10:40:49 +0000

    Correct random line generator courtesy of rjpgt

--- Unit 2 - Nodejs On The Command Line/U2-M5-chuck/chuck3.js
+++ Unit 2 - Nodejs On The Command Line/U2-M5-chuck/chuck3.js
@@ -7,7 +7,7 @@ exports.getChuckNorrisFact = (cb) => {
 	// A message of the day
 	let chuckCounter = 0;
 
-	const chuckLine = Math.floor(Math.random() * (86)) + 1;
+	const chuckLine = Math.floor(Math.random() * 86);
 
 	const rl = readline.createInterface({
 	    input: fs.createReadStream('chuck.txt')
